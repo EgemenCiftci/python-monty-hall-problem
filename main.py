85c0d4083ec13d2ff4ffb69ec56336aa1c769e56
Egemen Çiftci <egemenciftci@gmail.com>
2023-04-10 09:30:30 +0200

    Added types.

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 import random
 
-def getRandomDoor(doors):
+
+def getRandomDoor(doors) -> int:
     return random.choice(doors)
 
-def getRandomDoorExcept(doors, prizeDoor, selectedDoor):
-    return random.choice(list(filter(lambda f : (f != prizeDoor and f != selectedDoor), doors)))
 
-def calculateChance(isSwitchDoor):
-    doors = [0,1,2]
-    sampleCount = 100000
-    a = 0
+def getRandomDoorExcept(doors, prizeDoor, selectedDoor) -> int:
+    return random.choice(list(filter(lambda f: (f != prizeDoor and f != selectedDoor), doors)))
+
+
+def calculateChance(isSwitchDoor) -> float:
+    doors: list[int] = [0, 1, 2]
+    sampleCount: int = 100000
+    a: int = 0
     for i in range(sampleCount):
         prizeDoor = getRandomDoor(doors)
         selectedDoor = getRandomDoor(doors)
@@ -17,11 +20,12 @@ def calculateChance(isSwitchDoor):
 
         if isSwitchDoor:
             selectedDoor = getRandomDoorExcept(doors, selectedDoor, emptyDoor)
-        
+
         isWin = prizeDoor == selectedDoor
         if isWin:
             a += 1
     return a * 100 / sampleCount
+
 
 print("Win Chance (Stay): " + str(calculateChance(False)) + "%")
 print("Win Chance (Switch): " + str(calculateChance(True)) + "%")
